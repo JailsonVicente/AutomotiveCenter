dbf15c73bde6ca9c90eb3075a1228765e8bba3b8
Jailson Vicente <jailsonvicente188@gmail.com>
2023-03-05 13:34:29 -0300

    todas as alterações

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault();
+
     const nav = document.getElementById('nav');
+
     nav.classList.toggle('active');
+
     const active = nav.classList.contains('active');
+
     event.currentTarget.setAttribute('aria-expanded', 'active');
+
     if (active) {
         event.currentTarget.setAttribute('arial-label', 'Fechar Menu');
     } else {
         event.currentTarget.setAttribute('arial-label', 'Abrir Menu');
     }
 }
-
 
 
 btnMobile.addEventListener('click', toggleMenu);
@@ -38,3 +42,4 @@ function nextImage() {
 }
 
 // SLIDER SCRIPT
+
